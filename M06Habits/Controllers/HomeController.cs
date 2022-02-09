f3898ad803cdf9b514c34dbebaa4f3989510d70e
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2022-02-09 16:38:30 -0700

    controller base

--- M06Habits/Controllers/HomeController.cs
+++ M06Habits/Controllers/HomeController.cs
@@ -11,14 +11,69 @@ namespace M06Habits.Controllers
 {
     public class HomeController : Controller
     {
-
-        public HomeController()
+        private ContextBoundObject _context { get; set; }
+        public HomeController(Context x)
         {
+            _context = x;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTask(ApplicationResponse response)
+        {
+            //check validity of inputs
+            if (ModelState.IsValid)
+            {
+                _context.Add(response);
+                _context.SaveChanges();
+                return View("Index");
+            }
+            //show errors if not valid
+            else
+            {
+                ViewBag.Categories = _context.Categories.ToList();
+                return View(response);
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+
+            var task = _context.responses.Single(x => x.id == id);
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Edit(ApplicationResponse response)
+        {
+            _context.Update(response);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var task = _context.responses.Single(x => x.id == id);
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(ApplicationResponse response)
+        {
+            _context.responses.Remove(response);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
