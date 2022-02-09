9eaa580319435a5a746a138808630a781d4a70d0
kamryn-prock <90482936+kamryn-prock@users.noreply.github.com>
2022-02-09 16:34:05 -0700

    build the models and set up the database. populate the database with any needed info. configure all the needed settings/services/endpoints in the Startup.cs file.

--- M06Habits/Controllers/HomeController.cs
+++ M06Habits/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using M06Habits.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,14 +12,59 @@ namespace M06Habits.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext myContext { get; set; }
 
-        public HomeController()
+        public HomeController(DataContext someName)
         {
+            myContext = someName;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        // get data
+
+        public IActionResult MyApp()
+        {
+            ViewBag.Categories = myContext.Categories.ToList();
+
+            return View();
+        }
+
+        [HttpPost]
+        // post data when valid
+
+        public IActionResult MyApp(ApplicationResponse ar)
+        {
+                if (ModelState.IsValid)
+                {
+                    myContext.Add(ar);
+                    myContext.SaveChanges();
+
+                    return View("confirmation", ar);
+                }
+                else
+                {
+
+                    ViewBag.Categories = myContext.Categories.ToList();
+                    return View(ar);
+                }
+
+        }
+
+        //List View
+        public IActionResult List()
+        {
+            var applications = myContext.Responses
+                .Include(x => x.category)
+                .ToList()
+
+                .ToList();
+
+            return View(applications); // return list of applications
         }
     }
 }
